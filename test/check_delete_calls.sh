2ca92eb76a6a3c53c0796a1c372616c9be4bca2a
debug <debug@dbbdf1f2-f3a0-11dc-8807-f0e41a653009>
2010-01-31 13:06:06 +0000

    grep may output localized messages... (hack)
    git-svn-id: https://gxemul.svn.sourceforge.net/svnroot/gxemul/gxemul/trunk@5663 dbbdf1f2-f3a0-11dc-8807-f0e41a653009

--- test/check_delete_calls.sh
+++ test/check_delete_calls.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 rm -f tmp_delete.out
-grep -R "delete " src|grep -v \\.svn|cut -d \: -f 1|sort|uniq |grep -v "inary file" > tmp_delete.out 2> /dev/null
+LC_ALL=C grep -R "delete " src|grep -v \\.svn|cut -d \: -f 1|sort|uniq |grep -v "inary file" > tmp_delete.out 2> /dev/null
 
 ANYERRORS=0
 
